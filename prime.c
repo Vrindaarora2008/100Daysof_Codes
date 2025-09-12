8515c19f1d509ddf20388282121b9cc2655a1e7b
Vrindaarora2008 <vrindaaroraa83@gmail.com>
2025-09-12 09:51:22 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -8,12 +8,12 @@ temp=n;
 for(int i=1;i<=n-1;i++)
 if(temp%i==0)
 {
-    printf("It is not prime");
+    printf("It is prime");
     break;
 }
 else
 {
-    printf("It is prime");
+    printf("It is not prime");
 }
     return 0;
 }
