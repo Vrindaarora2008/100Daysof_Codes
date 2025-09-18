fec4906f6d8bc680a3213c14d801f7a54b6ac62d
Vrindaarora2008 <vrindaaroraa83@gmail.com>
2025-09-18 23:40:37 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,28 @@
+/*Q57: Find the sum of array elements.
+Sample Test Cases:
+Input 1:
+4
+2 4 6 8
+Output 1:
+20
+
+Input 2:
+3
+1 1 1
+Output 2:
+3*/
+#include<stdio.h>
+int main()
+{
+int n,sum=0;
+printf("Enter the size of elements\n");
+scanf("%d", &n);
+int arr[n];
+for(int i=0;i<n;i++)
+{
+    scanf("%d", &arr[i]);
+    sum=sum+arr[i];
+}
+printf("The sum is-%d", sum);
+    return 0;
+}
