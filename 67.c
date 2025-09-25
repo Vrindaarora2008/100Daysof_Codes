9fe8073b8df063bb667c08fb4f3b44b5819a1696
Vrindaarora2008 <vrindaaroraa83@gmail.com>
2025-09-25 12:39:39 +0530

    Add files via upload

--- 67.c
+++ 67.c
@@ -6,35 +6,33 @@ Input 1:
 2 15
 Output 1:
 10 20 15 30 40*/
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{
-    int n,element,pos;
+ {
+    int n, element, pos;
     printf("Enter size of array\n");
     scanf("%d", &n);
-    int arr[n];
-for(int a=0;a<n;a++)
-{
-    scanf("%d", &arr[a]);
-}
-printf("The array is-\n");
-for(int a=0;a<n;a++)
-{
-    printf("%d", arr[a]);
-    printf(" ");
-}
-printf("\nEnter new element and position\n");
-scanf("%d %d", &element, &pos);
-for(int a=n;a>pos;a--)
-{
-    arr[a]=arr[a-1];
-}
-arr[pos]=element;
-printf("The array is-\n");
-for(int i=0;i<n;i++)
-{
-printf("%d", arr[i]);
-printf(" ");
-}
-return 0;
+    int arr[n+1];
+    for (int a = 0; a < n; a++) 
+    {
+        scanf("%d", &arr[a]);
+    }
+    printf("The array is:\n");
+    for (int a = 0; a < n; a++)
+     {
+        printf("%d ", arr[a]);
+    }
+    printf("\nEnter new element and position\n");
+    scanf("%d %d", &element, &pos);
+    for (int i = n; i > pos; i--) 
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[pos] = element;
+    printf("The new array is:\n");
+    for (int a = 0; a <= n; a++) 
+    {
+        printf("%d ", arr[a]);
+    }
+    return 0;
 }
