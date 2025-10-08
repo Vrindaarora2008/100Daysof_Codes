5d09e6b314a192bdab0dd2b3f22f3e5a52f085ce
Vrindaarora2008 <vrindaaroraa83@gmail.com>
2025-10-08 16:28:16 +0530

    Add files via upload

---
+++ reversenonchange.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+ {
+    char str[200];
+    printf("Enter a sentence: ");
+    gets(str);
+    int len = strlen(str), start = 0, end;
+    for (int i=0;i<=len;i++)
+     {
+        if (str[i] == ' ' || str[i] == '\0')
+         {
+             end = i - 1;
+            while (start < end)
+             {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+                start++;
+                end--;
+            }
+            start = i + 1;
+        }
+    }
+    printf("Output: %s", str);
+    return 0;
+}
